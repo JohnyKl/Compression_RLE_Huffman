6d501a7cad38be3014ef7d382bc2f9dca3bfa7ef
JohnyKl <svintusiatko@gmail.com>
2015-02-22 14:00:31 +0200

    Compression RLE and Huffman
    Compression with RLE algorithm at first and Huffman algorithm next.

---
+++ KZI_lab_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+
+namespace KZI_lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+
+            StreamReader sr = new StreamReader("input_file.txt", Encoding.ASCII);
+            while (sr.Peek() != -1)
+            {
+                input = sr.ReadToEnd();     
+            }
+
+            input = RLE.encode(input);
+
+            Console.Write("RLE encoded: " + input);   
+
+            HuffmanTree huffmanTree = new HuffmanTree();
+
+            // Build the Huffman tree
+            huffmanTree.Build(input);
+
+            // Encode
+            BitArray encoded = huffmanTree.Encode(input);
+            
+            Console.Write("Huffman encoded: ");            
+            foreach (bool bit in encoded)
+            {
+
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+
+            File.WriteAllBytes("output_file.txt", ToByteArray(encoded));
+
+            // Decode
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.WriteLine("Huffman decoded: " + decoded);
+
+            decoded = RLE.decode(decoded);
+
+            Console.Write("RLE decoded: " + decoded);  
+
+            Console.ReadLine();
+        }
+
+        public static byte[] ToByteArray(BitArray bits)
+        {
+            const int BYTE = 8;
+            int length = (bits.Count / BYTE) + ((bits.Count % BYTE == 0) ? 0 : 1);
+            var bytes = new byte[length];
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+
+                int bitIndex = i % BYTE;
+                int byteIndex = i / BYTE;
+
+                int mask = (bits[i] ? 1 : 0) << bitIndex;
+                bytes[byteIndex] |= (byte)mask;
+
+            }//for
+
+            return bytes;
+
+        }//ToByteArray
+
+    }
+}
---
+++ KZI_lab_1/RLE.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZI_lab_1
+{
+    class RLE
+    {
+        public static string encode(string text)
+        {
+            string str1 = text, str = "", ch = "";
+
+            int i, k, j;
+            for (i = 0; i < str1.Length; )
+            { // от 0 до длины строки
+                ch = str1.Substring(i, 1); // получаем текущий символ из строки str1
+                k = 0; //счетчик количества повторяющихся символов
+                if (i == str1.Length - 1)
+                { // если последний символ
+                    str += Convert.ToString(ch);
+                    break; //выходим из цикла
+                }
+
+                if (str1.Substring(i + 1, 1) == ch)
+                {
+                    for (j = i; j < str1.Length; j++)
+                    {
+                        if (str1.Substring(j, 1) == ch) //если текущий символ равен символу из строки ch
+                            k++; //увеличиваем счетчик
+                        else
+                            break; //выходим из цикла
+                    }
+                    i = j;
+                }
+                else
+                    i++;
+
+                if (k != 0)
+                    str += Convert.ToString(k) + Convert.ToString(ch);
+                else
+                    str += Convert.ToString(ch);
+            }
+
+            return str;
+        }
+
+        public static string decode(string text)
+        {
+            string str1 = text, str = "", ch = "", s = "", symb = "";
+            int i, k = 0, j;
+            for (i = 0; i < str1.Length; )
+            {
+                ch = str1.Substring(i, 1); // текущий символ i
+                k = 0;
+                s = "";
+                if ("0123456789".Contains(ch))
+                { // если символ ch является цифрой
+                    for (j = i; j < str1.Length; j++)
+                    {
+                        if ("0123456789".Contains(str1.Substring(j, 1)))
+                        { // если текущий символ j является цифрой
+                            s += str1.Substring(j, 1);
+                        }
+                        else
+                            break;
+                    }
+                    symb = str1.Substring(j, 1); // получаем букву
+                    i = j + 1;
+                }
+                else
+                    i++;
+
+                if (s.Length != 0)
+                {
+                    for (j = 0; j < Convert.ToInt32(s); j++) // декодирование буквы
+                        str += symb;
+                }
+                else
+                    str += Convert.ToString(ch);
+            }
+            return str;
+        }
+    }
+}
